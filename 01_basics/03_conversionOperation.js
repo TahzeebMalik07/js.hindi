b4dea2d2297da956f33422e853eb033d311f6206
TahzeebMalik07 <tahzeebmalik7@gmail.com>
2025-08-14 19:52:57 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "malik"
+
+//console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+  
+
+// "33" => 33
+// "33abc" => Nan
+//true => 1 , false=> 0
+ 
+let isloggedIn = "malik"
+
+let booleanloggedIn = Boolean(isloggedIn)
+//console.log(booleanloggedIn);
+
+
+//1 => true, 0 => false
+//" " => false
+//"malik" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********************Operation*****************************/
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
